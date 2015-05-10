a3b2b65e63fadb04a264f5d51265c0569fc960d3
Nao Iizuka <iizuka@kyu-mu.net>
2015-05-10 23:57:09 +0900

    IRDeviceAPIService: Update doc

--- irkit-android-sdk/src/main/java/com/getirkit/irkit/net/IRDeviceAPIService.java
+++ irkit-android-sdk/src/main/java/com/getirkit/irkit/net/IRDeviceAPIService.java
@@ -7,19 +7,54 @@ import retrofit.http.POST;
 import retrofit.mime.TypedInput;
 
 /**
- * Device HTTP API
+ * IRKit Device HTTP APIのインタフェースです。
+ * Interface for IRKit Device HTTP API.
+ *
+ * <p>Example:
+ * <pre><code>
+ * IRHTTPClient httpClient = IRKit.sharedInstance().getHTTPClient();
+ *
+ * // Set base URL
+ * httpClient.setDeviceAPIEndpoint("http://192.168.1.1"); // IRKit IP address
+ * // Or from IRPeripheral
+ * httpClient.setDeviceAPIEndpoint(peripheral.getDeviceAPIEndpoint());
+ *
+ * // Get Device HTTP API service
+ * IRDeviceAPIService deviceAPI = httpClient.getDeviceAPIService();
+ *
+ * // Request parameters
+ * IRDeviceAPIService.PostMessagesRequest req = new IRDeviceAPIService.PostMessagesRequest();
+ * req.format = "raw";
+ * req.freq = 38.0f;
+ * req.data = new int[] {
+ *     18031, 8755, 1190, 1190, 1190, // ...
+ * };
+ *
+ * // Send POST /messages
+ * deviceAPI.postMessages(req, new Callback<IRDeviceAPIService.PostMessagesResponse>() {
+ *     {@literal @}Override
+ *     public void success(IRDeviceAPIService.PostMessagesResponse postMessagesResponse, Response response) {
+ *         // Success
+ *     }
+ *
+ *     {@literal @}Override
+ *     public void failure(RetrofitError error) {
+ *         // Error
+ *     }
+ * });
+ * </code></pre></p>
+ *
+ * @see <a href="http://getirkit.com/#IRKit-Device-API">IRKit Device HTTP API</a>
  */
 public interface IRDeviceAPIService {
-    public static class PostWifiResponse {
-    }
-
-    public static class PostMessagesRequest {
-        public String format;
-        public float freq;
-        public int[] data;
-    }
-
-    public static class PostMessagesResponse {
+    /**
+     * postKeys()のレスポンスです。
+     * Response of postKeys().
+     *
+     * @see #postKeys(Callback)
+     */
+    class PostKeysResponse {
+        public String clienttoken;
     }
 
     /**
@@ -31,34 +66,10 @@ public interface IRDeviceAPIService {
     void postKeys(Callback<PostKeysResponse> callback);
 
     /**
-     * 最も新しい受信した赤外線信号を返します。
+     * getMessages()のレスポンスです。
+     * Response of getMessages().
      *
-     * Long pollingは不可。
-     *
-     * @param callback
-     */
-    @GET("/messages")
-    void getMessages(Callback<GetMessagesResponse> callback);
-
-    /**
-     * 赤外線信号を送ります。
-     *
-     * @param request
-     * @param callback
-     */
-    @POST("/messages")
-    void postMessages(@Body PostMessagesRequest request, Callback<PostMessagesResponse> callback);
-
-    /**
-     * IRKitをWi-Fiに接続させる。
-     *
-     * @param callback
-     */
-    @POST("/wifi")
-    void postWifi(@Body TypedInput body, Callback<PostWifiResponse> callback);
-
-    /**
-     * Response of getMessages
+     * @see #getMessages(Callback)
      */
     class GetMessagesResponse {
         public String format;
@@ -67,9 +78,57 @@ public interface IRDeviceAPIService {
     }
 
     /**
-     * Response of postKeys
+     * 最も新しい受信した赤外線信号を返します。Long pollingには対応していません。
+     *
+     * @param callback
      */
-    class PostKeysResponse {
-        public String clienttoken;
+    @GET("/messages")
+    void getMessages(Callback<GetMessagesResponse> callback);
+
+    /**
+     * postMessages()のリクエストパラメータです。
+     * Request parameters for postMessages().
+     *
+     * @see #postMessages(PostMessagesRequest, Callback)
+     */
+    public static class PostMessagesRequest {
+        public String format;
+        public float freq;
+        public int[] data;
     }
+
+    /**
+     * postMessages()のレスポンスです。
+     * Response of postMessages().
+     *
+     * @see #postMessages(PostMessagesRequest, Callback)
+     */
+    public static class PostMessagesResponse {
+    }
+
+    /**
+     * 赤外線信号を送信します。
+     *
+     * @param request
+     * @param callback
+     */
+    @POST("/messages")
+    void postMessages(@Body PostMessagesRequest request, Callback<PostMessagesResponse> callback);
+
+    /**
+     * postWifi()のレスポンスです。
+     * Response of postWifi().
+     *
+     * @see #postWifi(TypedInput, Callback)
+     */
+    public static class PostWifiResponse {
+    }
+
+    /**
+     * IRKitをWi-Fiに接続させます。
+     *
+     * @param callback
+     */
+    @POST("/wifi")
+    void postWifi(@Body TypedInput body, Callback<PostWifiResponse> callback);
 }
